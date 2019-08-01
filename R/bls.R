c6c1b719e745bb9b0fbc78a5f302cef4a74d5a12
Andrew <arenn@design.upenn.edu>
2019-07-31 22:30:25 -0400

    Commit from RStudio

---
+++ R/bls.R
@@ -0,0 +1,41 @@
+library(readxl)
+library(glue)
+library(janitor)
+
+##
+## (https://www.bls.gov/lau/#cntyaa)
+##
+
+jobs <-
+  reduce(
+    map(10:16, function(x) {
+      glue("~/Desktop/R/git/twentysixteen/data-in/bls/laucnty{x}.xlsx") %>%
+        read_xlsx(skip = 4) %>%
+        clean_names() %>%
+        select(-code, -county_name_state_abbreviation, -x_1, -year) %>%
+        rename(statefp = code_1,
+               countyfp = code_2,
+               rate = percent) %>%
+        mutate(year = glue("20{x}"))
+    }), 
+    bind_rows
+  )
+
+##
+
+glimpse(jobs)
+
+##
+
+clean <-
+  jobs %>%
+  mutate(employed = as.numeric(str_replace(employed, pattern = ",", replacement = "")),
+         unemployed = as.numeric(str_replace(unemployed, pattern = ",", replacement = "")),
+         force = as.numeric(str_replace(force, pattern = ",", replacement = ""))) %>%
+  mutate(GEOID = paste(statefp, countyfp, sep = "")) %>%
+  select(GEOID, year, force, employed, unemployed, rate) %>%
+  filter(GEOID != "NANA")
+
+##
+
+write_csv(clean, "jobs.csv")
---
+++ R/census.R
@@ -0,0 +1,186 @@
+library(tidyverse)
+library(tidycensus)
+
+##
+
+move <- get_estimates(geography = "county",
+                      variables = "RNETMIG", 
+                      year = 2016) %>%
+  rename(migration = value) %>%
+  select(-variable)
+
+##
+
+vars <- load_variables(year = 2016, dataset = "acs5", cache = TRUE)
+
+test <- filter(vars, str_detect(concept, "EMPLOYMENT STATUS"))
+test <- filter(vars, str_detect(label, "Unemployed"))
+test <- filter(vars, str_detect(name, "B23001_001"))
+
+read_csv("https://www.bls.gov/lau/laucnty16.txt", skip = 3) %>% glimpse()
+
+##
+
+area <- 
+  get_acs(geography = "county", 
+          variables = c("B19083_001", "B06011_001", "B25077_001", "B25113_001"), 
+          year = 2016, output = "wide") %>% 
+  rename(gini = B19083_001E,
+         earn = B06011_001E,
+         home = B25077_001E,
+         rent = B25113_001E) %>%
+  select(GEOID, gini, earn, home, rent)
+
+##
+
+race <- 
+  get_acs(geography = "county", 
+          variables = c("B02001_002", "B02001_003"), 
+          year = 2016, output = "wide") %>% 
+  rename(white = B02001_002E,
+         black = B02001_003E) %>%
+  select(GEOID, white, black)
+
+##
+
+work <-
+  get_acs(geography = "county", variables = c("C18120_002E", "C18120_006E", "C18120_003E"),
+          year = 2016, output = "wide") %>%
+  rename(force = C18120_002E,
+         unemployed = C18120_006E,
+         employed = C18120_003E) %>%
+  select(GEOID, force, unemployed, employed)
+
+##
+
+live <- 
+  get_acs(geography = "county", variables = c("B01001_001", "B11016_001", "B01002_001E", "B06009_005E"),
+          year = 2016, output = "wide") %>%
+  rename(peeps = B01001_001E,
+         holds = B11016_001E,
+         age = B01002_001E,
+         edu = B06009_005E) %>%
+  select(GEOID, peeps, holds, age, edu)
+
+##
+
+fors <- get_acs(geography = "county", variables = c("B05006_124", "B05006_001"),
+                year = 2016, output = "wide") %>%
+  rename(latinos = B05006_124E,
+         foreign = B05006_001E) %>%
+  select(GEOID, latinos, foreign)
+
+##
+
+outs <- 
+  mobility <- get_acs(geography = "county", variables = c("B07001_033", "B07001_081"),
+                      year = 2016, output = "wide") %>%
+  rename(nation = B07001_033E,
+         abroad = B07001_081E) %>%
+  select(GEOID, nation, abroad)
+
+##
+
+pres <-
+  move %>%
+  left_join(area) %>%
+  left_join(live) %>%
+  left_join(work) %>%
+  left_join(race) %>%
+  left_join(fors) %>%
+  left_join(outs)
+
+##
+
+area <- 
+  get_acs(geography = "county", 
+          variables = c("B19083_001", "B06011_001", "B25077_001", "B25113_001"), 
+          year = 2010, output = "wide") %>% 
+  rename(gini = B19083_001E,
+         earn = B06011_001E,
+         home = B25077_001E,
+         rent = B25113_001E) %>%
+  select(GEOID, gini, earn, home, rent)
+
+##
+
+race <- 
+  get_acs(geography = "county", 
+          variables = c("B02001_002", "B02001_003"), 
+          year = 2010, output = "wide") %>% 
+  rename(white = B02001_002E,
+         black = B02001_003E) %>%
+  select(GEOID, white, black)
+
+##
+
+vars <- load_variables(year = 2010, dataset = "acs5", cache = TRUE)
+
+test <- filter(vars, str_detect(concept, "EMPLOYMENT"))
+
+work <-
+  get_acs(geography = "county", variables = c("C18120_002E", "C18120_006E", "C18120_003E"),
+          year = 2010, output = "wide") %>%
+  rename(force = C18120_002E,
+         unemployed = C18120_006E,
+         employed = C18120_003E) %>%
+  select(GEOID, force, unemployed, employed)
+
+##
+
+live <- 
+  get_acs(geography = "county", variables = c("B01001_001", "B11016_001", "B01002_001E", "B06009_005E"),
+          year = 2010, output = "wide") %>%
+  rename(peeps = B01001_001E,
+         holds = B11016_001E,
+         age = B01002_001E,
+         edu = B06009_005E) %>%
+  select(GEOID, peeps, holds, age, edu)
+
+##
+
+fors <- get_acs(geography = "county", variables = c("B05006_124", "B05006_001"),
+                year = 2010, output = "wide") %>%
+  rename(latinos = B05006_124E,
+         foreign = B05006_001E) %>%
+  select(GEOID, latinos, foreign)
+
+##
+
+outs <- 
+  get_acs(geography = "county", variables = c("B07001_033", "B07001_081"),
+                      year = 2010, output = "wide") %>%
+  rename(nation = B07001_033E,
+         abroad = B07001_081E) %>%
+  select(GEOID, nation, abroad)
+
+##
+
+past <-
+  move %>%
+  left_join(area) %>%
+  left_join(live) %>%
+  left_join(work) %>%
+  left_join(race) %>%
+  left_join(fors) %>%
+  left_join(outs)
+
+##
+
+migration <- reduce(
+  map(2015:2017, function(x) {
+    get_estimates(geography = "county",
+                  variables = "RNETMIG", 
+                  year = x) %>%
+      rename(migration = value) %>%
+      select(-variable) %>%
+      mutate(year = x)
+  }), 
+  bind_rows
+)
+
+##
+
+
+
+
