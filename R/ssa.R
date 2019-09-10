64bce9269885cc51b1b2f43c64064cc45699bb4f
Andrew <arenn@design.upenn.edu>
2019-09-10 16:27:31 -0400

    Commit from RStudio

---
+++ R/ssa.R
@@ -0,0 +1,22 @@
+library(readxl)
+library(tidyverse)
+library(janitor)
+
+##
+
+ssi <- read_excel("data-in/ssa/ssi_c16.xlsx") %>%
+  clean_names() %>%
+  remove_empty(c("rows", "cols"))
+
+##
+
+ssi %>%
+  transmute(GEOID = if_else(fips < 10000, paste("0", fips, sep = ""), paste(fips)),
+            ssi_recipients = ssi_recipients) %>%
+  left_join(population) %>%
+  select(1, 2, population) %>%
+  mutate(ssi_rate = (ssi_recipients / population) * 100) %>%
+  select(-population) %>%
+  write_csv("ssi.csv")
+
+##
